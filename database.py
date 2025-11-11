fc09c47baf8def2f842f1cec792f07ccb5bbfe36
Bhushan Patil <bjpatil1994@gmail.com>
2025-11-11 17:40:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Aapke 'amruno_db' database se connect kar rahe hain
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/amruno_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@containers-us-west-xx.railway.app:3306/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
